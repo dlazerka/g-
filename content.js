42be3405b126dbd94d426268ce13f3e7b3503507
Dzmitry Lazerka <dlazerka@gmail.com>
2012-09-03 14:41:16 -0700

    Moved domain check to manifest.

--- content.js
+++ content.js
@@ -1,6 +1,3 @@
-if (location.hostname.indexOf('plus.google.com') &&
-    location.hostname.indexOf('plus.sandbox.google.com')) {
-  var styleEl = document.createElement('style');
-  styleEl.innerText = '#gbi1a { display: none!important; }'
-  document.head.appendChild(styleEl);
-}
+var styleEl = document.createElement('style');
+styleEl.innerText = '#gbi1a { display: none!important; }';
+document.head.appendChild(styleEl);
