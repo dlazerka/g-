e6a29952d252760d341c10dfdb285e23b7505cca
Dzmitry Lazerka <dlazerka@gmail.com>
2012-09-03 14:50:54 -0700

    Added comment.

--- content.js
+++ content.js
@@ -1,3 +1,4 @@
+// Cannot inject a css file because it disrespects exclude_matches in manifest.
 var styleEl = document.createElement('style');
 styleEl.innerText = '#gbi1a { display: none!important; }';
 document.head.appendChild(styleEl);
