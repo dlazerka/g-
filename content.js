fccd9483c7f247aae3d538fcbf7d0e1587f1ca64
Dzmitry Lazerka <dlazerka@gmail.com>
2012-09-03 14:24:04 -0700

    Fixed no-work while loading GMail in new tab.

--- content.js
+++ content.js
@@ -1,13 +1,6 @@
-function seekAndDestroy() {
-  var el = document.getElementById('gbi1a');
-  if (el && location.hostname.indexOf('plus.google.com')) {
-    el.style.display = 'none';
-    return true;
-  }
-  return false;
-}
-
-if (!seekAndDestroy()) {
-  // Hack for GMail, anyway it's gonna show progress bar for a second. 
-  window.setTimeout(seekAndDestroy, 100);
+if (location.hostname.indexOf('plus.google.com') &&
+    location.hostname.indexOf('plus.sandbox.google.com')) {
+  var styleEl = document.createElement('style');
+  styleEl.innerText = '#gbi1a { display: none!important; }'
+  document.head.appendChild(styleEl);
 }
