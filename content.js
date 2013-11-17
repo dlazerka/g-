cd15ec9fb9345251f57a0f42a364df437c3a31ed
Dzmitry Lazerka <dlazerka@gmail.com>
2013-11-17 11:38:40 -0800

    Old, fixed youtube.

--- content.js
+++ content.js
@@ -1,8 +1,8 @@
 // Cannot inject a css file because it disrespects exclude_matches in manifest.
 var styleEl = document.createElement('style');
-var selector = '#gbg1, #gbgs3';
+var selector = '#gbg1';
 if (location.hostname.indexOf('youtube.com') > 0) {
-  selector = '#sb-button-notify, #sb-button-share';
+  selector = '#sb-button-notify';
 }
 styleEl.innerText = selector + ' {display: none!important; }';
 document.head.appendChild(styleEl);
