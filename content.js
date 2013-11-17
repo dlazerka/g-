4e596c03c6dc2262824b4fd30516e5b6ecc479a8
Dzmitry Lazerka <dlazerka@gmail.com>
2013-11-17 12:16:45 -0800

    Updated for changed website CSS.

--- content.js
+++ content.js
@@ -1,8 +1,7 @@
 // Cannot inject a css file because it disrespects exclude_matches in manifest.
 var styleEl = document.createElement('style');
-var selector = '#gbg1';
-if (location.hostname.indexOf('youtube.com') > 0) {
-  selector = '#sb-button-notify';
-}
-styleEl.innerText = selector + ' {display: none!important; }';
+var selector = '.gb_qa .gb_pa'; // red box
+selector +=  ', .gb_qa .gb_ra, .gb_qa .gb_ra.gb_sa'; // number in red box
+selector += ', #yt-masthead-user .sb-notif-on .yt-uix-button-content'; // youtube
+styleEl.innerText = selector + ' {visibility: hidden; }';
 document.head.appendChild(styleEl);
