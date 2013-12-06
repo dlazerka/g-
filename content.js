17e1fdc0e36852fd0eb0425e02f6edffa08e63df
Dzmitry Lazerka <dlazerka@gmail.com>
2013-12-06 11:10:14 -0800

    Updated CSS selector so that Google's releases won't break us.

--- content.js
+++ content.js
@@ -1,7 +1,7 @@
 // Cannot inject a css file because it disrespects exclude_matches in manifest.
 var styleEl = document.createElement('style');
-var selector = '.gb_qa .gb_pa'; // red box
-selector +=  ', .gb_qa .gb_ra, .gb_qa .gb_ra.gb_sa'; // number in red box
+var selector = 'a[href^="https://plus.google.com/u/"][href*="notifications/all"] div:nth-child(2)'; // red box
+selector += ', ' + selector + ' div'; // number in red box
 selector += ', #yt-masthead-user .sb-notif-on .yt-uix-button-content'; // youtube
 styleEl.innerText = selector + ' {visibility: hidden !important; }';
 document.head.appendChild(styleEl);
