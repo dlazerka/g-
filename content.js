3398cb8f4c35a688d22bd1c0ecd8a84807695ed9
Dzmitry Lazerka <dlazerka@gmail.com>
2014-10-17 12:57:58 -0700

    Updated selectors that stopped working yesterday.

--- content.js
+++ content.js
@@ -1,7 +1,6 @@
 // Cannot inject a css file because it disrespects exclude_matches in manifest.
 var styleEl = document.createElement('style');
-var selector = 'a[href^="https://plus.google.com/u/"][href*="notifications/all"] div:nth-child(2)'; // red box
-selector += ', ' + selector + ' div'; // number in red box
+var selector = '.gb_Oa, .gb_Oa div'; // red box
 selector += ', #yt-masthead-user .sb-notif-on .yt-uix-button-content'; // youtube
 styleEl.innerText = selector + ' {visibility: hidden !important; }';
 document.head.appendChild(styleEl);
